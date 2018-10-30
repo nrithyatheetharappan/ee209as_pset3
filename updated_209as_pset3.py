35f968bd10296a117dffebf96da30e89b4e8c122
Alexie <anpogue@g.ucla.edu>
2018-10-29 22:27:17 -0700

    Commit for pull

--- updated_209as_pset3.py
+++ updated_209as_pset3.py
@@ -23,14 +23,14 @@ class car_simulation:
         self.phi_2 = phi_2
         self.L = L
     def get_simulation(self):
-        #change standard deviation
-        #make them global? thetas?
+        # change standard deviation
+        # make them global? thetas?
         # get t.. t = t + something?
         w_omega_t = np.random.normal(0,1)
         w_v_t = np.random.normal(0,1)
         w_t = ((self.r*self.phi_1) - (self.r*self.phi_2))/self.L
         v_t = ((self.r*self.phi_1) + (self.r*self.phi_2))/2
-        #time evolution
+        # time evolution
         theta_t_measured = theta_t_measured + ((w_t + w_omega_t) * dt)
         x_t__measured = x_t_measured + ((v_t + w_v_t) * math.cos(theta_t_measured) * dt)
         y_t_measured = y_t_measured + ((v_t + w_v_t) * math.sin(theta_t_measured) * dt)
@@ -41,7 +41,7 @@ class car_simulation:
 class EKF(object):
     def __init__(self,d,phi,phi_1,phi_2,dt,L,Q,R):
         self.dt = dt
-        #self.intial_state = intial_state
+        # self.intial_state = intial_state
         self.d = d
         self.phi = phi
         self.phi_1 = phi_1
@@ -57,7 +57,7 @@ class EKF(object):
         
     def time_propogation_update(self):
     # measurement_values and estimated values are numpy arrays
-    #the ones with the bar, previous estimated values or prev finalized values?
+    # the ones with the bar, previous estimated values or prev finalized values?
         theta_t = self.X_cap[0][0]
         x_t = self.X_cap[1][0]
         y_t = self.X_cap[2][0]
@@ -67,12 +67,12 @@ class EKF(object):
 
         self.X_bar = [theta_t_plus_one, x_t_plus_one, y_t_plus_one]
     def time_linearization(self):
-        #move initializations out of the funtions
+        # move initializations out of the funtions
         theta_t = self.X_bar[0][0]
         self.F_t = find_F_t(self.F_t,theta_t, self.v_t)
         
         self.W_t = find_W_t(self.W_t, theta_t)
-        #noise_matrix = some guassian
+        # noise_matrix = some guassian
 #         linearized_matrix = np.dot(F_t,estimated_state) + np.dot(W_t,noise_matrix)
 #         return linearized_matrix
     def covariance_update(self):
@@ -82,36 +82,36 @@ class EKF(object):
         sigma_t_plus_one_temp1 = np.dot(self.W_t,self.Q)
         sigma_t_plus_one_temp1 = np.dot(sigma_t_plus_one_temp1,self.W_t.transpose())
         self.sigma_bar = sigma_t_plus_one_temp + sigma_t_plus_one_temp1
-    #def observation model     
-    def observation_linearization():
+    # def observation model
+    def observation_linearization(self):
     
-        self.H_t = find_H_t(self.H_t,self.observation,self.X_bar,landmark_values)
+        self.H_t = find_H_t(self.H_t, self.observation, self.X_bar, landmark_values)
         #np.dot(H_t,estimated_state) + velocities? 
     def kalman_gain_value(self):
-        #R = expectation of measurement noise
-        #estimated state or linearizered matrix?
-        #unpack values
-        #use H_t from linearization?
-        #H_t = np.zeros((2,3))
-        #H_t = find_H_t(H_t,observation,estimated_state,landmark_values)
-        inner_temp = np.dot(self.H_t,self.sigma_bar)
-        inner_temp = np.dot(inner_temp,self.H_t.transpose())
+        # R = expectation of measurement noise
+        # estimated state or linearizered matrix?
+        # unpack values
+        # use H_t from linearization?
+        # H_t = np.zeros((2,3))
+        # H_t = find_H_t(H_t,observation,estimated_state,landmark_values)
+        inner_temp = np.dot(self.H_t, self.sigma_bar)
+        inner_temp = np.dot(inner_temp, self.H_t.transpose())
         inner_temp = inner_temp + self.R
         inner_temp = np.linalg.inv(inner_temp)
-        outer_temp = np.dot(self.sigma_bar,self.H_t.transpose())
+        outer_temp = np.dot(self.sigma_bar, self.H_t.transpose())
         self.kalman_gain = np.dot(inner_temp, outer_temp)
-        #return gain
-    #def error
+        # return gain
+    # def error
     def observation_update(self):
         temp_product = np.dot(self.kalman_gain,error)
         self.X_cap = self.X_bar + temp_product
-        #return updated_state
+        # return updated_state
     def covariance_observation_update(self):
-        #take Ht out?
+        # take Ht out?
         inner_product = np.dot(self.kalman_gain,self.H_t)
         inner_product = np.dot(inner_product,self.sigma_bar)
         self.sigma_cap = self.sigma_bar - inner_product
-        #return updated_covariance
+        # return updated_covariance
     def run_EKF(self):
         self.time_propogation_update()
         self.time_linearization()
@@ -125,7 +125,7 @@ class EKF(object):
         return self.X_cap,self.sigma_cap
 
     
-#all below values?
+# all below values?
 
 # this can go in main
 import numpy as np
@@ -134,9 +134,9 @@ ekf = EKF(d,phi,phi_1,phi_2,dt,L,Q,R)
 car_trajectory = []
 estimation_trajectory = []
 for i in range(0,200):
-    #car_trajectory.append(call car simulation)
-    #call sensor simulation
-    #check for time step to compute estimate?
+    # car_trajectory.append(call car simulation)
+    # call sensor simulation
+    # check for time step to compute estimate?
     estimation_trajectory.append(ekf.run_ekf)
     
 
